9973e7a6c600b1fdd50a7081649e78fb47126d8f
Chad Smith <cssmith07060@gmail.com>
2021-02-08 20:30:25 -0500

    commit tree f655fb645b884b731d6865c7b7f256af6a6b122e

--- grandma.rb
+++ grandma.rb
@@ -1,13 +1,19 @@
 # Write a speak_to_grandma method.
-
+def speak_to_grandma(phrase)
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
-
+if phrase == phrase.downcase
 # If you shout, she can hear you (or at least she thinks so) 
 # and yells back
-
+elsif phrase == phrase.downcase
 # NO, NOT SINCE 1938!
-
+return "HUH?! SPEAK UP, SONNY!"
+elsif phrase == downcase
+    return "HUH? ! SPEAK UP, SONNY!"
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
-# 'I LOVE YOU TOO PUMPKIN!'
+# 'I LOVE YOU TOO PUMPKIN!
+end
+
+speak_to_grandma("HELLO THERE")
+
